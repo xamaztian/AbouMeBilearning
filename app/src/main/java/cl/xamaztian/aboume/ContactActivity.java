f82df1b8da2f3ba3ffae475b01304fed492c92ac
xamaztian <sebastian.gonzalez.bugueno@gmail.com>
2018-03-19 22:45:50 -0300

    first commit

---
+++ app/src/main/java/cl/xamaztian/aboume/ContactActivity.java
@@ -0,0 +1,13 @@
+package cl.xamaztian.aboume;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
---
+++ app/src/main/java/cl/xamaztian/aboume/EmailFragment.java
@@ -0,0 +1,45 @@
+package cl.xamaztian.aboume;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EmailFragment extends Fragment {
+
+
+    public EmailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_email, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button contactBtn = view.findViewById(R.id.contactBtn);
+        contactBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Funcionó", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
